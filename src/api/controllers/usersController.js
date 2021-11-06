a33432ec4ba117d005e8c528c2241e780abb7ef0
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-11-06 14:28:15 +0700

    Add API register new user

--- src/api/controllers/usersController.js
+++ src/api/controllers/usersController.js
@@ -8,7 +8,14 @@ const router = express.Router();
 const usersService = require('../services/usersService');
 const service = new usersService();
 
-// Get all
+// Register a new user
+router.post('/register', (req, res) => {
+    let params = req.body;
+    service.register(params).then((data) => res.status(201).send(data)).
+        catch(err => res.status(400).send({ success: false, data: [], message: err.message }));
+});
+
+// Create user for admin
 router.post('/', private, (req, res) => {
     let params = req.body;
     service.create(params).then((data) => res.status(200).send(data)).
